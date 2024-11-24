ba52dea45c290ce32c7bba2e71fa9241547a2253
Thiago Santos <thilego418@gmail.com>
2024-11-23 22:38:38 -0300

    Removendo atributo "Pergunta" de PublicarRequest Model e ajustando exemplos de blocos da página de vídeos

--- CastCradleDesktopApp/Features/Model/PublicarRequest.cs
+++ CastCradleDesktopApp/Features/Model/PublicarRequest.cs
@@ -5,7 +5,6 @@ namespace CastCradleDesktopApp.Features.Model
         public int? Id { get; set; }
         public required string Titulo { get; set; }
         public required string Descricao { get; set; }
-        public required string Pergunta { get; set; }
         public required string TipoMedia { get; set; }
         public required string TipoConteudo { get; set; }
         public Stream? Arquivo { get; set; }
